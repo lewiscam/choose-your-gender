3207b21b8dbf5debbe8b17b59630a3ec3c202404
Cameron Lewis <whitediamondmusic@gmail.com>
2025-09-28 16:12:36 -0400

    adding deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
+    apiKey: 'AIzaSyB6cXCxTGP1-OdHZehEqZ_MwzyaHjwZyvI',
+    authDomain: 'choose-your-gender.firebaseapp.com',
+    projectId: 'choose-your-gender',
+    storageBucket: 'choose-your-gender.firebasestorage.app',
+    messagingSenderId: '854616224321',
+    appId: '1:854616224321:web:1d6443ef1c36175ae28cc0',
+    measurementId: 'G-E1JGKSJLBE',
   },
   production: false,
 };
